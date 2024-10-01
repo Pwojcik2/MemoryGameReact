a407e6d72951fa565abdf19d06d4c0d948c8911f
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-10-01 15:48:07 -0700

    Card Component props adjustments

--- memory-game-proj/src/components/CardComponent.js
+++ memory-game-proj/src/components/CardComponent.js
@@ -1,16 +1,23 @@
-import './CardComponent.css'
+import "./CardComponent.css";
 
-export default function CardComponent({ card }) {
-    return(
-        <>
-        {card.map(card => (
-            <div className='card' key={card.id}>
-              <div>
-                <img className='front' src={card.src} alt='card'></img>
-                <img className='cover' src='/images/cover.png' alt='cover'></img>
-              </div>
-            </div>
-          ))}
-        </>
-    )
+export default function CardComponent({ card, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <>
+      <div className="card" key={card.id}>
+        <div>
+          <img className="front" src={card.src} alt="card"></img>
+          <img
+            className="cover"
+            onClick={handleClick}
+            src="/images/cover.png"
+            alt="cover"
+          ></img>
+        </div>
+      </div>
+    </>
+  );
 }
