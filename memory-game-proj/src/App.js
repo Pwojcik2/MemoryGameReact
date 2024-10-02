2f6f795b0395ec48c524a28f42cadc9743630431
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-10-01 17:43:33 -0700

    Added cards matching & not matching logic

--- memory-game-proj/src/App.js
+++ memory-game-proj/src/App.js
@@ -1,33 +1,64 @@
-
-import { useState } from 'react';
-import './App.css';
-import CardComponent from './components/CardComponent';
+import { useEffect, useState } from "react";
+import "./App.css";
+import CardComponent from "./components/CardComponent";
 
 function App() {
-
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const intialCards = [
-    {'src' : '/images/blue.png'},
-    {'src' : '/images/green.png'},
-    {'src' : '/images/orange.png'},
-    {'src' : '/images/purple.png'},
-    {'src' : '/images/red.png'},
-    {'src' : '/images/yellow.png'}
-  ]
+    { src: "/images/blue.png" },
+    { src: "/images/green.png" },
+    { src: "/images/orange.png" },
+    { src: "/images/purple.png" },
+    { src: "/images/red.png" },
+    { src: "/images/yellow.png" },
+  ];
 
+  //shuffle cards
   const shuffleCards = () => {
-    const shuffledCards = [... intialCards, ...intialCards]
+    const shuffledCards = [...intialCards, ...intialCards]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }))
+      .map((card) => ({ ...card, id: Math.random() }));
 
-    setCards(shuffledCards)
-  }
+    setCards(shuffledCards);
+  };
+
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //compare 2 cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("MATCHED");
+        resetTurn();
+      } else {
+        console.log("NO MATCH");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  //reset choices
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+  };
 
   return (
     <div className="App">
-      <div className='card-grid'>
-        <CardComponent card={cards}/>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <CardComponent
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+          />
+        ))}
       </div>
       <button onClick={shuffleCards}>Reset</button>
     </div>
